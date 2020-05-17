446ae225331c57f7da2c1e3d92ae7a5021f9e8d6
lv fenghui <fenghui_lv.sgmyt@saic-gm.com>
2020-05-17 22:42:08 +0800

    任务完成

---
+++ src/main/resources/sql_script/sql_initial.sql
@@ -0,0 +1,15 @@
+﻿DROP TABLE IF EXISTS `carsinventory`;
+CREATE TABLE `carsinventory` (
+  `id` int(100) NOT NULL,
+  `carcount` int(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+
+
+-- Records of carmessage
+-- ----------------------------
+INSERT INTO `carsinventory` VALUES ('2', '10');
+INSERT INTO `carsinventory` VALUES ('3', '23');
+INSERT INTO `carsinventory` VALUES ('7', '8');
+INSERT INTO `carsinventory` VALUES ('11', '2');
